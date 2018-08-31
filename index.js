bc5db4770be59e51e903e7e7e3ab0189806d0c56
qwer11234 <1529833211@qq.com>
2018-08-31 13:05:44 +0800

    1

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const server = express();
 server.get('/',(req,res) => {
-    res.send('打开的是傻瓜');
-
-
+    res.send('a');
 });
-server.listen(80)
+server.listen(2000)
